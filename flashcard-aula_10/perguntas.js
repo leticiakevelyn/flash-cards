b558c51aca2b365b27d198993df27d3fd208a2a9
leticiakevelyn <160230880+leticiakevelyn@users.noreply.github.com>
2024-09-18 08:00:05 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,77 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'geografia',
+    'qual a capital da frança?',
+    'brasília'
+)
+
+criaCartao(
+    'história',
+    'quem foi o responsável pela unificação da Alemanha?',
+    'Otto von Bismarck'
+)
+
+criaCartao(
+    'inglẽs',
+    'o que significa a expressão break a leg?',
+    'para desejar boa sorte'
+)
+
+criaCartao(
+    'programação',
+    'o que é uma variável em programação?',
+    'um espaço de memória que armazena dados que podem ser alterados'
+)
+
+criaCartao(
+    'matemática',
+    'como calcula a área de um triângulo?',
+    'usando a fórmula: A= (base x altura) /2'
+)
+
+criaCartao(
+    'ciências',
+    'qual é a função das mitocôndrias nas células?',
+    'são responsáveis pela produção de energia, convertendo nutrientes em ATP'
+)
+
+criaCartao(
+    'geografia',
+    'o que caracteriza um bioma de tundra',
+    'por temperaturas extremamente baixas, solo permafrost e uma vegetação escassa'
+)
+
+criaCartao(
+    'geografia',
+    'qual a capital da frança?',
+    'brasília'
+)
+
+criaCartao(
+    'geografia',
+    'qual a capital da frança?',
+    'brasília'
+)
